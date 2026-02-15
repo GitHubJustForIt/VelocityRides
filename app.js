c38b5a0c36549f22b4304ae777aade999b1ffefd
GitHubJustForIt <151542280+GitHubJustForIt@users.noreply.github.com>
2026-02-15 16:56:00 +0100

    app.js

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
+const grid = document.getElementById("grid");
+const userDisplay = document.getElementById("userDisplay");
 
+
+function initApp(){
+
+  const user = localStorage.getItem("user");
+
+  userDisplay.innerHTML =
+    `<i class="fa-solid fa-user"></i> ${user}`;
+
+  render(projects);
+}
+
+
+/* RENDER */
+
+function render(list){
+
+  grid.innerHTML="";
+
+  list.forEach((p,i)=>{
+
+    const card = document.createElement("div");
+
+    card.className="card";
+
+    card.innerHTML=`
+
+      <img src="${p.image}">
+
+      <h3>${p.title}</h3>
+
+      <p>${p.description}</p>
+
+    `;
+
+    grid.appendChild(card);
+
+  });
+
+}
