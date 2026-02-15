e9d439641ac1f85c4a44f4661042f118b27e41bb
GitHubJustForIt <151542280+GitHubJustForIt@users.noreply.github.com>
2026-02-15 17:06:49 +0100

    data.js

--- data.js
+++ data.js
@@ -1,23 +1,33 @@
-const projects=[
+// Discord Webhook
+const WEBHOOK_URL = "https://discordapp.com/api/webhooks/1472624952917364998/dLUkhFwa2ZyEhrNbOHfwyRe3ufr8BtwzgH_kcni2fgtugwfaABMOq3vwdPTzfqJ9Q2OE";
 
+// Projekte / Templates
+const projects = [
   {
     title:"Space Adventure",
-    description:"Explore galaxies.",
+    description:"Explore galaxies and planets.",
     price:"$9.99",
     gamepass:"Required",
     image:"img/space.jpg",
     purchased:false,
     buyer:""
   },
-
   {
     title:"Racing Pro",
-    description:"Fast racing game.",
+    description:"Fast racing game with upgrades.",
     price:"Free",
     gamepass:"Not Required",
     image:"img/race.jpg",
     purchased:true,
     buyer:"Max"
+  },
+  {
+    title:"Zombie World",
+    description:"Survive zombies and collect resources.",
+    price:"$4.99",
+    gamepass:"Required",
+    image:"img/zombie.jpg",
+    purchased:false,
+    buyer:""
   }
-
 ];
