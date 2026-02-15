23e438a5fc33ff5dd3f92b95c27fe1aba68c1077
GitHubJustForIt <151542280+GitHubJustForIt@users.noreply.github.com>
2026-02-15 16:55:09 +0100

    login.js

--- login.js
+++ login.js
@@ -1,1 +1,35 @@
+const loginBox = document.getElementById("loginBox");
+const app = document.getElementById("app");
 
+const loginBtn = document.getElementById("loginBtn");
+const usernameInput = document.getElementById("usernameInput");
+
+loginBtn.addEventListener("click", login);
+
+function login(){
+
+  const name = usernameInput.value.trim();
+
+  if(!name){
+    alert("Enter username");
+    return;
+  }
+
+  localStorage.setItem("user", name);
+
+  startApp();
+}
+
+/* AUTO LOGIN */
+
+if(localStorage.getItem("user")){
+  startApp();
+}
+
+function startApp(){
+
+  loginBox.style.display="none";
+  app.style.display="block";
+
+  initApp();
+}
