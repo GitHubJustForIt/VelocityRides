72af005d0339d5df7965af36f821152101919344
GitHubJustForIt <151542280+GitHubJustForIt@users.noreply.github.com>
2026-02-15 16:54:29 +0100

    data.js

--- data.js
+++ data.js
@@ -1,1 +1,23 @@
+const projects=[
 
+  {
+    title:"Space Adventure",
+    description:"Explore galaxies.",
+    price:"$9.99",
+    gamepass:"Required",
+    image:"img/space.jpg",
+    purchased:false,
+    buyer:""
+  },
+
+  {
+    title:"Racing Pro",
+    description:"Fast racing game.",
+    price:"Free",
+    gamepass:"Not Required",
+    image:"img/race.jpg",
+    purchased:true,
+    buyer:"Max"
+  }
+
+];
